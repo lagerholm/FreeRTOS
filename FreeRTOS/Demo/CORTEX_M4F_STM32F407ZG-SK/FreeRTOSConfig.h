ffb63d56722a8ecf96fb6671214f0e7b5bbdaad7
Anton Lagerholm <anton@lagerholm.se>
2022-04-22 09:23:57 +0200

    Fix spelling in FreeRTOSConfig.h demo

--- FreeRTOS/Demo/CORTEX_M4F_STM32F407ZG-SK/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_M4F_STM32F407ZG-SK/FreeRTOSConfig.h
@@ -89,7 +89,7 @@ to exclude the API function. */
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
-	/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
+	/* __NVIC_PRIO_BITS will be specified when CMSIS is being used. */
 	#define configPRIO_BITS       		__NVIC_PRIO_BITS
 #else
 	#define configPRIO_BITS       		4        /* 15 priority levels */
